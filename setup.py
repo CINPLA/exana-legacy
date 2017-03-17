0e5a38ca71484518c5e21270ceac364beeab0263
Mikkel E Lepperød <lep.mik@gmail.com>
2017-03-17 16:13:55 +0100

    added all analysis packages from expipe

---
+++ setup.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+import os
+
+from setuptools import setup, find_packages
+import versioneer
+
+
+long_description = open("README.md").read()
+
+install_requires = []
+
+setup(
+    name="exana",
+    packages=find_packages(),
+    include_package_data=True,
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass()
+)
