ecda885926ce1f80f8c6ea6f29944a7fcfdfdf4d
Mikkel Elle Lepperød <lep.mik@gmail.com>
2017-05-03 09:56:29 +0200

    auto atempts

--- docs/conf.py
+++ docs/conf.py
@@ -44,7 +44,7 @@ if not on_rtd:  # only import and set the theme if we're building docs locally
 # ones.
 extensions = [
     'sphinx.ext.autodoc',
-    'numpydoc'
+    'numpydoc',
     'sphinx.ext.doctest',
     'sphinx.ext.todo',
     'sphinx.ext.coverage',
