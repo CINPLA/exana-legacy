59dd198a4f2b5e2bd9ef1e90446a9f137a24a3f0
Mikkel Elle Lepperød <lep.mik@gmail.com>
2017-05-03 10:29:24 +0200

    docs

--- docs/conf.py
+++ docs/conf.py
@@ -21,8 +21,9 @@ import os
 import re
 import exana
 import sys
-sys.path.insert(0, os.path.abspath('../exana'))
-sys.path.insert(0, os.path.abspath('../'))
+sys.path.insert(0, os.path.abspath('.'))
+# sys.path.insert(0, os.path.abspath('../exana'))
+# sys.path.insert(0, os.path.abspath('../'))
 
 on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 if not on_rtd:  # only import and set the theme if we're building docs locally
