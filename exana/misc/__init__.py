1f593417d5bea10985d2e34de58a29394f6bf37c
Mikkel Elle Lepperød <lep.mik@gmail.com>
2017-05-03 11:14:18 +0200

    updates

--- exana/misc/__init__.py
+++ exana/misc/__init__.py
@@ -1,2 +1,2 @@
 from .plot import addticks, floating_axis, simpleaxis, upper_legend
-from .tools import concatenate_spiketrains, normalize
+from .tools import concatenate_spiketrains, normalize, is_quantities
