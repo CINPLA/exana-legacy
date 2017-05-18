fe54def3fe8eb0a879f897bfe1ad6426534032d7
alejoe91 <alejoe9187@gmail.com>
2017-05-18 16:21:03 +0200

    added intan, spike-stat in single plot, remove artifact

--- exana/waveform/plot.py
+++ exana/waveform/plot.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import quantities as pq
 
 
-def plot_waveforms(sptr, color='r', fig=None, title='waveforms', lw=2):
+def plot_waveforms(sptr, color='r', fig=None, title='waveforms', lw=2, gs=None, fs=8):
     """
     Visualize waveforms on respective channels
 
@@ -18,34 +18,61 @@ def plot_waveforms(sptr, color='r', fig=None, title='waveforms', lw=2):
     -------
     out : fig
     """
+    import matplotlib.gridspec as gridspec
+
     nrc = sptr.waveforms.shape[1]
     if fig is None:
         fig = plt.figure()
     axs = []
     ax = None
-    for c in range(nrc):
-        ax = fig.add_subplot(1, nrc, c+1, sharex=ax, sharey=ax)
-        axs.append(ax)
-    for c in range(nrc):
-        wf = sptr.waveforms[:, c, :]
-        m = np.mean(wf, axis=0)
-        stime = np.arange(m.size, dtype=np.float32)/sptr.sampling_rate
-        stime.units = 'ms'
-        sd = np.std(wf, axis=0)
-        axs[c].plot(stime, m, color=color, lw=lw)
-        axs[c].fill_between(stime, m-sd, m+sd, alpha=.1, color=color)
-        if sptr.left_sweep is not None:
-            sptr.left_sweep.units = 'ms'
-            axs[c].axvspan(sptr.left_sweep, sptr.left_sweep, color='k',
-                           ls='--')
-        axs[c].set_xlabel(stime.dimensionality)
-        axs[c].set_xlim([stime.min(), stime.max()])
-        if c > 0:
-            plt.setp(axs[c].get_yticklabels(), visible=False)
-    axs[0].set_ylabel(r'amplitude $\pm$ std [%s]' % m.dimensionality)
-    fig.suptitle(title)
+    if gs is None:
+        for c in range(nrc):
+            ax = fig.add_subplot(1, nrc, c+1, sharex=ax, sharey=ax)
+            axs.append(ax)
+        for c in range(nrc):
+            wf = sptr.waveforms[:, c, :]
+            m = np.mean(wf, axis=0)
+            stime = np.arange(m.size, dtype=np.float32)/sptr.sampling_rate
+            stime.units = 'ms'
+            sd = np.std(wf, axis=0)
+            axs[c].plot(stime, m, color=color, lw=lw)
+            axs[c].fill_between(stime, m-sd, m+sd, alpha=.1, color=color)
+            if sptr.left_sweep is not None:
+                sptr.left_sweep.units = 'ms'
+                axs[c].axvspan(sptr.left_sweep, sptr.left_sweep, color='k',
+                               ls='--')
+            axs[c].set_xlabel(stime.dimensionality)
+            axs[c].set_xlim([stime.min(), stime.max()])
+            if c > 0:
+                plt.setp(axs[c].get_yticklabels(), visible=False)
+        axs[0].set_ylabel(r'amplitude $\pm$ std [%s]' % m.dimensionality)
+        fig.suptitle(title)
+    else:
+        gs0 = gridspec.GridSpecFromSubplotSpec(1, nrc, subplot_spec=gs)
+        for c in range(nrc):
+            ax = fig.add_subplot(gs0[c], sharex=ax, sharey=ax)
+            axs.append(ax)
+        for c in range(nrc):
+            wf = sptr.waveforms[:, c, :]
+            m = np.mean(wf, axis=0)
+            stime = np.arange(m.size, dtype=np.float32)/sptr.sampling_rate
+            stime.units = 'ms'
+            sd = np.std(wf, axis=0)
+            axs[c].plot(stime, m, color=color, lw=lw)
+            axs[c].fill_between(stime, m-sd, m+sd, alpha=.1, color=color)
+            if sptr.left_sweep is not None:
+                sptr.left_sweep.units = 'ms'
+                axs[c].axvspan(sptr.left_sweep, sptr.left_sweep, color='k',
+                               ls='--')
+            axs[c].set_xlabel(stime.dimensionality, fontsize=fs)
+            axs[c].set_xticks([0, round(stime.max())])
+            axs[c].set_xticklabels([0, np.round(stime.max().magnitude)], fontsize=fs-2)
+            plt.setp(axs[c].get_yticklabels(), fontsize=fs-2)
+            if c > 0:
+                plt.setp(axs[c].get_yticklabels(), visible=False)
+        axs[0].set_ylabel(r'amplitude $\pm$ std [%s]' % m.dimensionality, fontsize=fs)
+        fig.suptitle(title)
     return fig
-
 
 def plot_largest_waveform(sptr, color='r', ax=None, title='waveforms', lw=2,
                           ylabel=True, xlabel=True):
@@ -88,7 +115,7 @@ def plot_largest_waveform(sptr, color='r', ax=None, title='waveforms', lw=2,
         ax.set_ylabel(r'amplitude $\pm$ std [%s]' % m.dimensionality)
 
 
-def plot_amp_clusters(sptrs, colors=None, fig=None, title=None):
+def plot_amp_clusters(sptrs, colors=None, fig=None, title=None, gs=None, fs=8):
     """
     Visualize clustering on amplitude at detection point
 
@@ -107,35 +134,66 @@ def plot_amp_clusters(sptrs, colors=None, fig=None, title=None):
     if fig is None:
         fig = plt.figure()
     import matplotlib.gridspec as gridspec
-    gs = gridspec.GridSpec(nrc-1, nrc-1)
-    axs = []
-    for x in range(nrc-1):
-        for y in range(nrc-1):
-            if y <= x:
-                ax = fig.add_subplot(gs[x, y])
-                axs.append(ax)
-                ax.set_xticks([])
-                ax.set_yticks([])
-                if x == nrc-2:
-                    ax.set_xlabel('channel %i' % (range(nrc)[y-1]))
-                if y == 0:
-                    ax.set_ylabel('channel %i' % (x))
-    if colors is None:
-        from matplotlib.pyplot import cm
-        colors = cm.rainbow(np.linspace(0, 1, len(sptrs)))
-    for idx, sptr in enumerate(sptrs):
-        cnt = 0
-        wf = sptr.waveforms
-        if sptr.left_sweep is None:
-            sptr.left_sweep = 0.2 * pq.ms
-        mask = int(sptr.sampling_rate*sptr.left_sweep.rescale('s'))
-        color = colors[idx]
+    if gs is None:
+        gs = gridspec.GridSpec(nrc-1, nrc-1)
+        axs = []
         for x in range(nrc-1):
             for y in range(nrc-1):
                 if y <= x:
-                    axs[cnt].plot(wf[:, y-1, mask], wf[:, x, mask], ls='None',
-                                  marker='.', color=color)
-                    cnt += 1
+                    ax = fig.add_subplot(gs[x, y])
+                    axs.append(ax)
+                    ax.set_xticks([])
+                    ax.set_yticks([])
+                    if x == nrc-2:
+                        ax.set_xlabel('channel %i' % (range(nrc)[y-1]))
+                    if y == 0:
+                        ax.set_ylabel('channel %i' % (x))
+        if colors is None:
+            from matplotlib.pyplot import cm
+            colors = cm.rainbow(np.linspace(0, 1, len(sptrs)))
+        for idx, sptr in enumerate(sptrs):
+            cnt = 0
+            wf = sptr.waveforms
+            if sptr.left_sweep is None:
+                sptr.left_sweep = 0.2 * pq.ms
+            mask = int(sptr.sampling_rate*sptr.left_sweep.rescale('s'))
+            color = colors[idx]
+            for x in range(nrc-1):
+                for y in range(nrc-1):
+                    if y <= x:
+                        axs[cnt].plot(wf[:, y-1, mask], wf[:, x, mask], ls='None',
+                                      marker='.', color=color)
+                        cnt += 1
+    else:
+        gs0 = gridspec.GridSpecFromSubplotSpec(nrc-1, nrc-1, subplot_spec=gs)
+        axs = []
+        for x in range(nrc - 1):
+            for y in range(nrc - 1):
+                if y <= x:
+                    ax = fig.add_subplot(gs0[x, y])
+                    axs.append(ax)
+                    ax.set_xticks([])
+                    ax.set_yticks([])
+                    if x == nrc - 2:
+                        ax.set_xlabel('ch %i' % (range(nrc)[y - 1]), fontsize=fs)
+                    if y == 0:
+                        ax.set_ylabel('ch %i' % (x), fontsize=fs)
+        if colors is None:
+            from matplotlib.pyplot import cm
+            colors = cm.rainbow(np.linspace(0, 1, len(sptrs)))
+        for idx, sptr in enumerate(sptrs):
+            cnt = 0
+            wf = sptr.waveforms
+            if sptr.left_sweep is None:
+                sptr.left_sweep = 0.2 * pq.ms
+            mask = int(sptr.sampling_rate * sptr.left_sweep.rescale('s'))
+            color = colors[idx]
+            for x in range(nrc - 1):
+                for y in range(nrc - 1):
+                    if y <= x:
+                        axs[cnt].plot(wf[:, y - 1, mask], wf[:, x, mask], ls='None',
+                                      marker='.', color=color)
+                        cnt += 1
     if title is not None:
         fig.suptitle(title)
     return fig
