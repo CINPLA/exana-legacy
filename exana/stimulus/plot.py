57c09aad6f342951ae62f52312613de1b402c301
Milad <miladh@ibv.uio.no>
2017-06-01 18:56:28 +0200

    more visual analysis in exana

--- exana/stimulus/plot.py
+++ exana/stimulus/plot.py
@@ -9,16 +9,80 @@ from ..general.plot import (plot_raster)
 from ..statistics.tools import (fano_factor_linregress)
 
 
-def orient_raster_plots(trials, save_path=None):
-    # TODO: add save fig
+def polar_tuning_curve(orients, rates, ax=None, params={}):
+    """
+    Direction polar tuning curve
+    """
+    import numpy as np
+    import math
+    from exana.misc import pretty_plotting
+
+    assert len(orients) == len(rates)
+
+    if ax is None:
+        fig, ax = plt.subplots()
+        ax = plt.subplot(111, projection='polar')
+
+    ax.plot(orients, rates, '-', **params)
+    ax.fill(orients, rates, alpha=1)
+    ax.set_yticklabels([])
+    ax.set_theta_zero_location('N')
+    ax.set_theta_direction(-1)
+
+    return ax
+
+
+def plot_tuning_overview(trials, spontan_rate=None):
+    """
+    Makes orientation tuning plots (line and polar plot)
+    for each stimulus orientation.
+
+    Parameters
+    ----------
+    trials : list
+        list of neo.SpikeTrain
+    spontan_rates : defaultdict(dict), optional
+        rates[channel_index_name][unit_id] = spontaneous firing rate trials.
+    """
+    import seaborn
+    from exana.stimulus.tools import (make_orientation_trials,
+                                      compute_orientation_tuning,
+                                      compute_osi)
+    fig = plt.figure()
+
+    ax1 = fig.add_subplot(1, 2, 1)
+    trials = make_orientation_trials(trials)
+    rates, orients = compute_orientation_tuning(trials)
+    preferred_orient, index = compute_osi(rates, orients)
+
+    ax1.set_title("Preferred orientation={},\n OSI={}".format(preferred_orient,
+                                                              round(index, 2)))
+    ax1.plot(orients, rates, "-o", label="with bkg")
+    ax1.set_xlabel("Orientation")
+    ax1.set_ylabel("Rate (1/s)")
+
+    ax2 = fig.add_subplot(1, 2, 2, projection="polar")
+    polar_tuning_curve(orients.rescale("rad"), rates, ax=ax2)
+
+    if spontan_rate is not None:
+        ax1.plot(orients, rates - spontan_rate, "-o", label="without bkg")
+        ax1.legend()
+
+    fig.tight_layout()
+
+    return fig
+
+
+def orient_raster_plots(trials):
     """
     Makes raster plot for each stimulus orientation
 
     Parameters
     ----------
-    trials : defaultdict(dict)
-        trials[channel_index_name][unit_id] = list of spike_train trials.
+    trials : list
+        list of neo.SpikeTrain
     """
+    import seaborn
     orient_trials = make_orientation_trials(trials)
     col_count = 4
     row_count = int(np.ceil(len(orient_trials))/col_count)
@@ -27,11 +91,8 @@ def orient_raster_plots(trials, save_path=None):
         ax = fig.add_subplot(row_count, col_count, i+1)
         ax = plot_raster(trials, ax=ax)
         ax.set_title(orient)
-        ax.grid()
+        ax.grid(False)
     fig.tight_layout()
-
-    if save_path is not None:
-        plt.savefig(save_path, fig)
 
     return fig
 
--- exana/tests/test_stimulus.py
+++ exana/tests/test_stimulus.py
@@ -3,10 +3,41 @@ import quantities as pq
 import pytest
 
 
+def test_wrap_angle_360():
+    from exana.stimulus.tools import (wrap_angle)
+    angles = np.array([85.26, -437.34, 298.14, 57.47, -28.98, 681.25, -643.99,
+                       43.71, -233.82, -549.63, 593.7, 164.48, 544.05, -52.66,
+                       79.87, -21.11, 708.31, 29.45, 279.14, -586.88])
+
+    angles_ex = np.array([85.26, 282.66, 298.14, 57.47, 331.02, 321.25, 76.01,
+                          43.71, 126.18, 170.37, 233.7, 164.48, 184.05, 307.34,
+                          79.87, 338.89, 348.31, 29.45, 279.14, 133.12])
+
+    result = wrap_angle(angles, 360)
+    np.testing.assert_almost_equal(result, angles_ex, decimal=13)
+
+
+def test_wrap_angle_2pi():
+    from exana.stimulus.tools import (wrap_angle)
+    angles = np.array([-7.15, -7.3, 7.74, 4.68, -9.33, 1.32, 4.18, 3.49,
+                       8.21, 1.43, -0.96, 6.63, 1.32, 9.66, -10.57, -7.17,
+                       1.84, -10.24, -7.31, -11.71, -1.82, 2.85, 1.99, -5.11,
+                       -10.16, 3.6, 9.36, -3.13, -0.64, -1.77])
+
+    angles_ex = np.array([5.41637061, 5.26637061, 1.45681469, 4.68, 3.23637061,
+                          1.32, 4.18, 3.49, 1.92681469, 1.43,
+                          5.32318531, 0.34681469, 1.32, 3.37681469, 1.99637061,
+                          5.39637061, 1.84, 2.32637061, 5.25637061, 0.85637061,
+                          4.46318531, 2.85, 1.99, 1.17318531, 2.40637061, 3.6, 3.07681469, 3.15318531, 5.64318531, 4.51318531])
+
+    result = wrap_angle(angles, 2*np.pi)
+    np.testing.assert_almost_equal(result, angles_ex, decimal=8)
+
+
 def test_make_stimulus_off_epoch():
     from neo.core import Epoch
     from exana.stimulus.tools import (make_stimulus_off_epoch)
-    
+
     times = np.linspace(0, 10, 11) * pq.s
     durations = np.ones(len(times)) * pq.s
     labels = np.ones(len(times))
