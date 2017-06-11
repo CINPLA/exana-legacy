4003c4a36a809944a7d398e8daa96d1fabb749bc
Mikkel E Lepperød <lep.mik@gmail.com>
2017-06-11 13:25:27 +0200

    docs and testing

--- exana/statistics/correlogram.py
+++ exana/statistics/correlogram.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Ahtuhor Chris Rodger
+Ahthor Chris Rodger
 
 Originally from OpenElectrophy, with the following licence information:
 
@@ -13,30 +13,63 @@ Originally from OpenElectrophy, with the following licence information:
 
 import numpy as np
 
-def correlogram(t1, t2=None, bin_width=.001, limit=.02, auto=False):
+def correlogram(t1, t2=None, bin_width=.001, limit=.02, auto=False,
+                density=False):
     """Return crosscorrelogram of two spike trains.
-    
-    Essentially, this algorithm subtracts each spike time in `t1` 
+
+    Essentially, this algorithm subtracts each spike time in `t1`
     from all of `t2` and bins the results with np.histogram, though
     several tweaks were made for efficiency.
-    
-    Arguments
+
+    Parameters
     ---------
-        t1 : first spiketrain, raw spike times in seconds.
-        t2 : second spiketrain, raw spike times in seconds.
-        bin_width : width of each bar in histogram in sec
-        limit : positive and negative extent of histogram, in seconds
-        auto : if True, then returns autocorrelogram of `t1` and in
-            this case `t2` can be None.
-    
+        t1 : np.array
+            First spiketrain, raw spike times in seconds.
+        t2 : np.array
+            Second spiketrain, raw spike times in seconds.
+        bin_width : float
+            Width of each bar in histogram in seconds.
+        limit : float
+            Positive and negative extent of histogram, in seconds.
+        auto : bool
+            If True, then returns autocorrelogram of `t1` and in
+            this case `t2` can be None. Default is False.
+        density : bool
+            If True, then returns the probability density function.
+
+    See also
+    --------
+    numpy.histogram : The histogram function in use.
+
     Returns
     -------
-        (count, bins) : a tuple containing the bin edges (in seconds) and the
-        count of spikes in each bin.
+        (count, bins) : tuple
+            a tuple containing the bin edges (in seconds) and the
+            count of spikes in each bin.
 
-        `bins` is relative to `t1`. That is, if `t1` leads `t2`, then
+    Note
+    ----
+        `bins` are relative to `t1`. That is, if `t1` leads `t2`, then
         `count` will peak in a positive time bin.
-    
+
+    Examples
+    --------
+    >>> t1 = np.arange(0, .5, .1)
+    >>> t2 = np.arange(0.1, .6, .1)
+    >>> limit = 1
+    >>> bin_width = .2
+    >>> counts, bins = correlogram(t1=t1, t2=t2, bin_width=bin_width,
+    ...                            limit=limit, auto=False)
+    >>> counts
+    array([ 0,  0,  0,  3,  3, 13,  5,  1,  0,  0])
+
+    The correlogram algorithm is identical to, but computationally faster than
+    the histogram of differences of each timepoint, i.e.
+    >>> diff = [t2 - t for t in t1]
+    >>> counts2, bins = np.histogram(diff, bins=bins)
+    >>> np.array_equal(counts2, counts)
+    True
+
     """
     # For auto-CCGs, make sure we use the same exact values
     # Otherwise numerical issues may arise when we compensate for zeros later
@@ -55,12 +88,11 @@ def correlogram(t1, t2=None, bin_width=.001, limit=.02, auto=False):
     # Determine the bin edges for the histogram
     # Later we will rely on the symmetry of `bins` for undoing `swap_args`
     limit = float(limit)
-    bins = np.linspace(-limit, limit, num=(2 * limit/bin_width + 1))
+    bins = np.linspace(-limit, limit, num=int(2 * limit/bin_width + 1))
 
-    # This is the old way to calculate bin edges. I think it is more
-    # sensitive to numerical error. The new way may slightly change the
-    # way that spikes near the bin edges are assigned.
-    #bins = np.arange(-limit, limit + bin_width, bin_width)
+    # The numpy.arange method overshoots slightly the edges i.e. bin_width + epsilon
+    # which leads to inclusion of spikes falling on edges.
+    # bins = np.arange(-limit, limit + bin_width, bin_width)
 
     # Determine the indexes into `t2` that are relevant for each spike in `t1`
     ii2 = np.searchsorted(t2, t1 - limit)
@@ -73,7 +105,7 @@ def correlogram(t1, t2=None, bin_width=.001, limit=.02, auto=False):
 
     # Actually do the histogram. Note that calls to np.histogram are
     # expensive because it does not assume sorted data.
-    count, bins = np.histogram(big, bins=bins)
+    count, bins = np.histogram(big, bins=bins, density=density)
 
     if auto:
         # Compensate for the peak at time zero that results in autocorrelations
