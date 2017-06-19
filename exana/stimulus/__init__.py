1cceada9bcfae9397ee68f1376aaec8ff90d1d3e
Mikkel E Lepperød <lep.mik@gmail.com>
2017-06-19 14:13:43 +0200

    fix merging issues after subrepo pull/merge

--- exana/stimulus/__init__.py
+++ exana/stimulus/__init__.py
@@ -2,4 +2,6 @@ from .plot import (plot_psth, plot_stimulus_overview, orient_raster_plots,
                    plot_tuning_overview, polar_tuning_curve)
 from .tools import (epoch_overview, make_analog_trials, make_spiketrain_trials,
                     get_epoch, make_stimulus_trials, make_stimulus_off_epoch,
-                    compute_spontan_rate, compute_osi)
+                    compute_spontan_rate, compute_osi, make_orientation_trials,
+                    _convert_quantity_scalar_to_string, _convert_string_to_quantity_scalar,
+                    compute_orientation_tuning)
