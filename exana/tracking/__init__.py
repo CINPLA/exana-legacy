cfc0f755c54e22c77e99e3a7477c30f9b131d2d0
Mikkel Elle Lepperød <lep.mik@gmail.com>
2017-06-30 16:30:39 +0200

    processed tracking function

--- exana/tracking/__init__.py
+++ exana/tracking/__init__.py
@@ -4,5 +4,5 @@ from .stats import (sparsity, selectivity, information_rate,
                     information_specificity, prob_dist)
 from .tools import (get_tracking, get_raw_position, select_best_position,
                     interp_filt_position, velocity_threshold,
-                    et_processed_position)
+                    get_processed_tracking)
 from .head import (head_direction_rate, head_direction_stats, head_direction)
